4b773e589b5e3b632610dbd69a7c553a4121a7b6
Evgeny Shevelev <sheveleveester@gmail.com>
2020-04-17 22:03:43 +0300

    test

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-a = int(input())
-b = int(input())
-print(a)
-print(b)
-c = int(input())
 
-d = c
+b = input("Введите имя ")
+a = int(input("Введите номер из списка "))
+names = ['egor', 'ivan', 'petr', b]
+names.reverse()
+print(names[a].title())
+print(names)
+counter = 0
+# while counter != len(names):
+#     print(names[counter].title())
+#     counter += 1
+
+for i in names:
+    print(i)
