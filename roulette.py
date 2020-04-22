da1f5907a12068b64fb4e26abdbca90002330dfc
Evgeny Shevelev <sheveleveester@gmail.com>
2020-04-22 21:47:46 +0300

    рулеточка усложненная

--- roulette.py
+++ roulette.py
@@ -1,14 +1,18 @@
 import random
-bullets = [0 for i in range(random.randint(0,100))]
-bullets[random.randint(0, len(bullets)-1)] = 1
-
-a = int(input("Хочешь сыграть в игру? Если да, то напиши 1б а если нет, то 0: "))
+a = int(input("Хочешь сыграть в игру? Если да, то напиши 1, а если нет, то 0: "))
 
 while a == 1:
+    bullets = [0 for i in range(random.randint(0, 100))]
+    counter_bullets = 0
+    number_bullets = random.randint(0, len(bullets) - 1)
+    while number_bullets > 0:
+        bullets[random.randint(0, len(bullets) - 1)] = 1
+        number_bullets -= 1
+        counter_bullets += 1
     if random.choice(bullets) == 1:
-        print("Ты проиграл! Хочешь сыграть еще раз?")
+        print("Ты проиграл! У тебя было " + str(counter_bullets) + " из " + str(len(bullets)) + " шансов проиграть!. Хочешь сыграть еще раз?")
     else:
-        print("Ты выиграл!")
+        print("Ты выиграл! У тебя было " + str(counter_bullets) + " из " + str(len(bullets)) + " шансов проиграть!. Хочешь сыграть еще раз?")
     a = int(input("Хочешь сыграть в игру? Если да, то напиши 1б а если нет, то 0: "))
 
 
